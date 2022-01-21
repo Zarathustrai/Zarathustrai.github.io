b7ab772b281ee570ea99cdf1f0d84cd3b514466c
evelVT <94784245+evelVT@users.noreply.github.com>
2022-01-21 12:11:38 +0100

    update test database

--- src/components/FetchBeers.js
+++ src/components/FetchBeers.js
@@ -10,7 +10,7 @@ export default class FetchBeers extends React.Component {
   }
 
   componentDidMount() {
-    const url = "http://localhost:3000/beers";
+    const url = "https://test-1bd08-default-rtdb.europe-west1.firebasedatabase.app/beers";
     fetch(url)
       .then(response => response.json())
       .then(beers => this.setState({beers}));
